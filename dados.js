1949ddeb55845da5d4a4e31cad02b72b5823f4fd
Fábio Leandro Medeiros <fabio.leandro.medeiros@gmail.com>
2024-09-06 20:09:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou um dos maiores nomes do skate mundial e um exemplo para jovens atletas.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas Olimpíadas de Paris, conquistando a medalha de ouro. Sua história de superação e dedicação ao esporte a tornou um símbolo de força e determinação.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "Ginástica ginastica"
+    }
+];
